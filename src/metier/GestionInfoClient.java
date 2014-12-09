bf27d734098abb87cf4a999012c2eb51798f3153
Dauchy Michael <michael.dauchy@etudiant.univ-lille1.fr>
2014-12-10 00:06:45 +0100

    Ajout de la méthode pour ajouter les points de fidélité d'un client après une réservation selon sa durée dans la classe métier GestionInfoClient.

--- src/metier/GestionInfoClient.java
+++ src/metier/GestionInfoClient.java
@@ -8,10 +8,9 @@ public class GestionInfoClient {
 
 	public void addPtFidelité (Client client, Reservation reservation){
 		int nbPtFideliteClient = client.getPointFidelite();
-		int nbHeure= (((int)reservation.getDateFin().getTime() -(int) reservation.getDateDebut().getTime())/(60*60*60));
+		int nbHeure= reservation.getNbHeure();
 		int nbPtFidelite = 5 * nbHeure;
 		client.setPointFidelite( nbPtFideliteClient + nbPtFidelite);
 	}
-
 
 }
