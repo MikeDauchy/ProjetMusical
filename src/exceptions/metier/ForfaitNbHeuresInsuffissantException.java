54be651dc81131e2e9a62fdfa502e49a71d4d504
Dauchy Michael <michael.dauchy@etudiant.univ-lille1.fr>
2014-12-10 00:09:17 +0100

    Creation de l'exception ForfaitNbHeuresInsuffisant pour contrôler que le nombre d'heures restant sur un forfait est suffisant, en fonction de la durée de la réservation.

---
+++ src/exceptions/metier/ForfaitNbHeuresInsuffissantException.java
@@ -0,0 +1,14 @@
+package exceptions.metier;
+
+public class ForfaitNbHeuresInsuffissantException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ForfaitNbHeuresInsuffissantException(String message) {
+		super(message);
+	}
+
+}
