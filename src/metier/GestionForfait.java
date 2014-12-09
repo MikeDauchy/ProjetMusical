3d1c4727a199c8b742f844b3668777505359764f
Dauchy Michael <michael.dauchy@etudiant.univ-lille1.fr>
2014-12-10 00:04:09 +0100

    Ajout de la méthode pour déduire le nombre d'heures sur le forfait dans la classe métier GestionForfait.

--- src/metier/GestionForfait.java
+++ src/metier/GestionForfait.java
@@ -1,6 +1,19 @@
 package metier;
 
+import java.sql.SQLException;
+
+import donnees.Client;
+import donnees.Forfait;
+import donnees.reservations.Reservation;
+import exceptions.accesAuDonnees.ObjetInconnu;
+
 public class GestionForfait {
+	
+	public void deductHeure(Reservation reservation , Forfait forfait){
+		int nbHeureReservation = reservation.getNbHeure();
+		int nbHeureForfait = forfait.getNbHeure() - nbHeureReservation;
+		forfait.setNbHeure(nbHeureForfait);
+	}
 
 
 }
