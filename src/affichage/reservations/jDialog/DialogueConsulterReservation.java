2ce86b9da9f1d61743273b594b327d5e1c3ddf23
Quentin <laujac.quentin@hotmail.fr>
2014-12-09 19:08:24 +0100

    QLA:ajout des boutons pour faire semaine précédente ou semaine suivante

--- src/affichage/reservations/jDialog/DialogueConsulterReservation.java
+++ src/affichage/reservations/jDialog/DialogueConsulterReservation.java
@@ -3,12 +3,16 @@ package affichage.reservations.jDialog;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dialog;
+import java.awt.GridLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -24,36 +28,117 @@ public class DialogueConsulterReservation extends JPanel{
 	private static final long serialVersionUID = 1L;
 
 	private Dialog dialog;
-	private PanelAgenda panelAgenda;
 
+	JButton btnSemainePrec = new JButton("Semaine précédente");
+	JButton btnSemaineSuiv = new JButton("Semaine suivante");
+	
 	JLabel labelTitre = new JLabel("Consultation des reservations", SwingConstants.CENTER);
+	JPanel panelEnTete = new JPanel();
+	JPanel panelContenu = new JPanel();
+	JPanel panelAgenda;
+	
+	Calendar dateDeReference;
 
 	public DialogueConsulterReservation(Dialog dialog) throws HeadlessException, SQLException, ObjetInconnu {
 		super();
-		this.setSize(800, 1000);
 		this.dialog = dialog;
 		setLayout( new BorderLayout());
-	
-		Calendar lundiDeReference = new java.util.GregorianCalendar();
-		lundiDeReference.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+		
+		
+		//creation de l'entete
+		creationListenerBtnPrec();
+		creationListenerBtnSuiv();
+		labelTitre.setForeground(Color.blue);
+		panelEnTete.setLayout(new GridLayout(1, 3));
+		panelEnTete.add(btnSemainePrec);
+		panelEnTete.add(labelTitre);
+		panelEnTete.add(btnSemaineSuiv);
+		//
+		add(panelEnTete, BorderLayout.NORTH);
+		
+		
+		
+		//Chargement du contenu du planning
+		dateDeReference = new java.util.GregorianCalendar();
+		dateDeReference.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
 		
 		Calendar dimancheDeReference = new java.util.GregorianCalendar();
-		dimancheDeReference.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-		dimancheDeReference.add(Calendar.DATE, 7);
+		dimancheDeReference.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
 				
 		List<Reservation> listReservation;
 		try {
-			listReservation = ReservationFactory.getInstance().listerByDates(lundiDeReference.getTime(), dimancheDeReference.getTime());
+			listReservation = ReservationFactory.getInstance().listerByDates(dateDeReference.getTime(), dimancheDeReference.getTime());
 		} catch (ObjetInconnu e) {
 			listReservation = new ArrayList<Reservation>();
 		}
-		panelAgenda = new PanelAgenda(listReservation, lundiDeReference);
+		panelAgenda = new PanelAgenda(listReservation, (Calendar)dateDeReference.clone());
+		panelContenu.add(panelAgenda);
+		//
+		add(panelContenu, BorderLayout.CENTER);
 		
-		add(labelTitre, BorderLayout.NORTH);
-		add(panelAgenda, BorderLayout.CENTER);
-		labelTitre.setForeground(Color.blue);
 
-		setVisible(true);
+		dialog.pack();
+		dialog.setLocationRelativeTo(null);
+	}
+	
+	private void recompacter(){
+		this.revalidate();
+		panelContenu.revalidate();
+		dialog.pack();
+		dialog.setLocationRelativeTo(null);
+	}
+	
+	private void creationListenerBtnPrec(){
+		btnSemainePrec.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ajouterSemaineAgenda(-1);
+			}
+		});
+	}
+	
+	private void creationListenerBtnSuiv(){
+		btnSemaineSuiv.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ajouterSemaineAgenda(1);
+			}
+		});
+	}
+	
+	private void ajouterSemaineAgenda(int nbSemaine){
+		//Chargement du contenu du planning
+		dateDeReference.add(Calendar.DATE, nbSemaine*7);
+		
+		Calendar lundi = (Calendar)dateDeReference.clone();
+		
+		Calendar dimanche = (Calendar)dateDeReference.clone();
+		dimanche.add(Calendar.DATE, 7);
+				
+		List<Reservation> listReservation = null;
+		try {
+			listReservation = ReservationFactory.getInstance().listerByDates(lundi.getTime(), dimanche.getTime());
+		} catch (ObjetInconnu oIe) {
+			listReservation = new ArrayList<Reservation>();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			panelContenu.removeAll();
+			panelAgenda = new PanelAgenda(listReservation, lundi);
+			panelContenu.add(panelAgenda);
+		} catch (ObjetInconnu e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		//
+		recompacter();
 	}
 
 
