2cbd43f6f87118cf69d4485d127e5b6645a0d3a5
Dauchy Michael <michael.dauchy@etudiant.univ-lille1.fr>
2015-01-05 00:37:32 +0100

    Modification du nom de la méthode addPtFidelite (Suppresion accent) + Ajout de la méthode deletePtFidelite pour déduire des points de fidelité et de la méthode deuxHeuresGratuite pour verifier si le client a accès au deux heures gratuites qui lui sont offert grâce à un nombre de points de fidelité supérieur ou égale à 150.

--- src/metier/GestionInfoClient.java
+++ src/metier/GestionInfoClient.java
@@ -6,11 +6,20 @@ import donnees.reservations.Reservation;
 public class GestionInfoClient {
 
 
-	public void addPtFidelité (Client client, Reservation reservation){
+	public void addPtFidelite (Client client, Reservation reservation){
 		int nbPtFideliteClient = client.getPointFidelite();
 		int nbHeure= reservation.getNbHeure();
 		int nbPtFidelite = 5 * nbHeure;
 		client.setPointFidelite( nbPtFideliteClient + nbPtFidelite);
+	}
+	
+	public void deletePtFidelite(Client client, int pointFidelite){
+		int nbPtFideliteClient = client.getPointFidelite();
+		client.setPointFidelite(nbPtFideliteClient - pointFidelite);
+	}
+	
+	public boolean deuxHeuresGratuite(Client client){
+		return client.getPointFidelite()>=150;
 	}
 
 }
