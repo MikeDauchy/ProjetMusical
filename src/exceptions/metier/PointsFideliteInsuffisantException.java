562773dd7060b3965a96bc61f3f546b939edf1d2
Dauchy Michael <michael.dauchy@etudiant.univ-lille1.fr>
2015-01-05 01:23:48 +0100

    Création de l'exception PointsFideliteInsuffisantException, permettant de dire si le nombre de points de fidélité est suffisant sur le compte du client.

---
+++ src/exceptions/metier/PointsFideliteInsuffisantException.java
@@ -0,0 +1,14 @@
+package exceptions.metier;
+
+public class PointsFideliteInsuffisantException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PointsFideliteInsuffisantException(String message) {
+		super(message);
+	}
+
+}
