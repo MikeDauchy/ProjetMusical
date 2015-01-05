2a96f0defa7b3ae3581dfeff07786fedeeb3c5d2
Dauchy Michael <michael.dauchy@etudiant.univ-lille1.fr>
2015-01-05 20:57:58 +0100

    Modification du format de la date et de la couleur des éléments selon si la réservation a été payé ou non.

--- src/affichage/informationsClient/listCellRenderer/ReservationsListCellRenderer.java
+++ src/affichage/informationsClient/listCellRenderer/ReservationsListCellRenderer.java
@@ -1,6 +1,9 @@
 package affichage.informationsClient.listCellRenderer;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.sql.SQLException;
+import java.text.DateFormat;
 
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
@@ -9,6 +12,7 @@ import javax.swing.ListCellRenderer;
 
 import donnees.Client;
 import donnees.reservations.Reservation;
+import exceptions.accesAuDonnees.ObjetInconnu;
 
 public class ReservationsListCellRenderer implements ListCellRenderer{
 
@@ -17,9 +21,20 @@ public class ReservationsListCellRenderer implements ListCellRenderer{
 	@Override
 	public Component getListCellRendererComponent(JList list, Object value, int index,
 			boolean isSelected, boolean cellHasFocus) {
-		JLabel renderer = (JLabel) defaultRenderer.getListCellRendererComponent(list,((Reservation)value).getDateDebut(), index,
+		DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL);
+		JLabel renderer = (JLabel) defaultRenderer.getListCellRendererComponent(list,dateFormat.format(((Reservation)value).getDateDebut()), index,
 				isSelected, cellHasFocus);
+
+		try {
+			if (!((Reservation)value).getFacture().isEstPaye()) renderer.setForeground(Color.blue);
+			else if((((Reservation)value).getFacture().isEstPaye())){
+				renderer.setForeground(Color.green);
+			}
+		} catch (ObjetInconnu | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return renderer;
 	}
-	
+
 }
