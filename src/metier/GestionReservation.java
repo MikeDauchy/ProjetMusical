fc14a5f21e4d5d07684ac1922da55db54e04ee03
Dauchy Michael <michael.dauchy@etudiant.univ-lille1.fr>
2015-01-06 01:53:28 +0100

    Ajout de la gestion des points de fidélité pour chaque réservations dans les différentes méthodes de paiement.

--- src/metier/GestionReservation.java
+++ src/metier/GestionReservation.java
@@ -1,13 +1,17 @@
 package metier;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import donnees.Client;
 import donnees.Facture;
 import donnees.Forfait;
 import donnees.reservations.Reservation;
 import exceptions.accesAuDonnees.ObjetInconnu;
 import exceptions.metier.ForfaitNbHeuresInsuffissantException;
 import exceptions.metier.PointsFideliteInsuffisantException;
+import fabriques.donnes.ClientFactory;
 import fabriques.donnes.FactureFactory;
 import fabriques.donnes.ForfaitFactory;
 import fabriques.donnes.ReservationFactory;
@@ -26,20 +30,36 @@ public class GestionReservation {
 
 	public void paiementReservationCB(Reservation reservation) throws ObjetInconnu, SQLException{
 		Facture facture =reservation.getFacture();
+		Client client = reservation.getFacture().getClient();
+		List<Reservation> lesReservation = facture.getListReservations();
 		if(!facture.isEstPaye()){
 			facture.setEstPaye(true);
 			FactureFactory.getInstance().update(facture);
+			for(Reservation r : lesReservation){
+				gestInfoClient.addPtFidelite(client, r);
+			}
+			ClientFactory.getInstance().update(client);
 		}
 	}
 
 	public void paiementReservationForfait(Reservation reservation,Forfait forfait) throws ObjetInconnu, SQLException{
 		Facture facture =reservation.getFacture();
+		Client client = reservation.getFacture().getClient();
+		int nbHeure =0;
 		if(!facture.isEstPaye()){
-			if(forfait.getNbHeure() > reservation.getNbHeure() || forfait.getNbHeure() !=0){
+			List<Reservation> lesReservation = facture.getListReservations();
+			for(Reservation r : lesReservation){
+				nbHeure += r.getNbHeure();
+			}
+			if(forfait.getNbHeure() > nbHeure || forfait.getNbHeure() !=0){
 				gestForfait.deductHeure(reservation ,forfait);
 				facture.setEstPaye(true);
+				for(Reservation r : lesReservation){
+					gestInfoClient.addPtFidelite(client, r);
+				}
 				FactureFactory.getInstance().update(facture);
 				ForfaitFactory.getInstance().update(forfait);
+				ClientFactory.getInstance().update(client);
 			}
 			else{
 				throw new ForfaitNbHeuresInsuffissantException("Nombre d'heure sur le forfait insuffisant");
